f3df4ec10e3c6bf2e7ef13815f954e26db5fff29
Develanton <develanton@gmail.com>
2018-05-17 14:15:51 -0500

    get started click working, moving on to settimeout and result page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,30 @@
 $(document).ready(function(){
     console.log("ready");
+
+
+
+var timer;
+
+var selec = $("#starter").on("click",function(){
+
+    $("#topsec").attr("style","display:none");
+    $("#trivia").attr("style","display");
+
+
+
+})
+   
+setTimeout(() => {
+        
+}, 30000);
+
+
+
+
+
+
+
+
 });
 
 
-function test(a) {
-    console.log("test");
-    
-}
